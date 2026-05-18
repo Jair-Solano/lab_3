04d93042b28ba23ef733305789281474be5f7f16
Jair-Solano <jair.solano@utp.ac.pa>
2026-05-18 17:47:46 -0500

    fix driver fixture conflict

--- test_tienda.py
+++ test_tienda.py
@@ -1,16 +1,9 @@
+
 import pytest
 from selenium import webdriver
 from login_page import LoginPage
 
-@pytest.fixture
-def driver():
-    """Configuración inicial del navegador"""
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
-
 def test_login_fallido(driver):
-    """Prueba de usuario bloqueado"""
     driver.get("https://saucedemo.com")
     login = LoginPage(driver)
     login.ingresar_credenciales("locked_out_user", "secret_sauce")
@@ -18,10 +11,8 @@ def test_login_fallido(driver):
     assert "locked out" in login.obtener_error()
 
 def test_login_exitoso(driver):
-    """Prueba de login correcto"""
     driver.get("https://saucedemo.com")
     login = LoginPage(driver)
     login.ingresar_credenciales("standard_user", "secret_sauce")
     login.click_login()
     assert "inventory.html" in driver.current_url
-    #assert "pagina_falsa.html" in driver.current_url
