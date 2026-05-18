85a4fe77dcd1e79d168ea901d454d57b521a4771
Jair-Solano <jair.solano@utp.ac.pa>
2026-05-18 17:43:45 -0500

    fix conftest syntax error

--- conftest.py
+++ conftest.py
@@ -1,30 +1,3 @@
-"""
-import pytest
-from selenium import webdriver
-import pytest_html
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    """Captura de pantalla automática en caso de fallo"""
-    outcome = yield
-    report = outcome.get_result()
-    extras = getattr(report, "extra", [])
-
-    if report.when == "call" and report.failed:
-        driver = item.funcargs['driver']
-        screenshot = driver.get_screenshot_as_base64()
-        html = f'<div><img src="data:image/png;base64,{screenshot}" alt="screenshot" style="width:304px;height:228px;" onclick="window.open(this.src)" align="right"/></div>'
-        extras.append(pytest_html.extras.html(html))
-        report.extra = extras
-
-@pytest.fixture
-def driver():
-    """Fixture del navegador"""
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    driver.quit()
-"""
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -53,4 +26,3 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.quit()
-    
